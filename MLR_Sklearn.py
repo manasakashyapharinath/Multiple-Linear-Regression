c03ea10278e9f2276d4fb635cbc11f4d4edebb45
Manasa Kashyap Harinath <manasakashyap@knights.ucf.edu>
2017-09-25 23:45:17 -0400

    Update MLR_Sklearn.py

--- MLR_Sklearn.py
+++ MLR_Sklearn.py
@@ -7,14 +7,18 @@ from sklearn.linear_model import LinearRegression
 from sklearn.cross_validation import KFold
 from sklearn.cross_validation import cross_val_score
 from sklearn import cross_validation 
+from sklearn.cross_validation import train_test_split
 
-
-XMat = pd.read_csv('predictors.csv')
+XMat = pd.read_csv('anotherxcel2.csv')
 XmatrixInti = XMat.as_matrix()
 X = pd.DataFrame(XmatrixInti)
-Y= pd.read_csv('response.csv')
+Y= pd.read_csv('anotherY2.csv')
 Ymatr=Y.as_matrix()
 linreg = LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(
+    X, Y,test_size=0.3, random_state=0)
+linreg.fit(X_train, y_train)
+p=linreg.predict(X_test)
 s = cross_validation.KFold(len(X), n_folds=5, shuffle=True, random_state=42)
 scores = cross_validation.cross_val_score(linreg, X, Y, cv=s, scoring='mean_squared_error')
 print('this is second one')
@@ -25,5 +29,6 @@ print('this is root mean squares ')
 print(rmse_scores)
 print('this is   average root mean squares ')
 print(rmse_scores.mean())
+
 
 
